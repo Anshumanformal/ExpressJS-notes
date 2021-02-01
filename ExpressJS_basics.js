4bc1855fd8a9884cf966fa88a3a2a0219aacb26d
Anshuman Ranjan <500062237@stu.upes.ac.in>
2021-02-01 15:39:00 +0530

    Create ExpressJS_basics.js

---
+++ ExpressJS_basics.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+//The callback function has two parameters.
+// req object - represents HTTP request.
+// res object - represents HTTP response that the Express App sends when it
+//              receives an HTTP request.
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Server can now respond.  Hello World!</h1>`);
+  //   console.log("Hello from the server.");
+});
+
+app.get("/about", (req, res) => {
+  res.send(`<h1>Responding from About Us page.</h1>`);
+});
+
+app.get("/contactUs", (req, res) => {
+  res.send(`<h1>Responding from Contact Us page.</h1>`);
+});
+
+app.listen(8000, "127.0.0.1", () => {
+  console.log("The server is listening.");
+});
