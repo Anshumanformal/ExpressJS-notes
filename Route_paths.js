d3ee8735ac54a082c3a2153b2e0aae0bd46e87a6
Anshuman Ranjan <500062237@stu.upes.ac.in>
2021-02-02 12:22:44 +0530

    Create Route_paths.js

---
+++ Route_paths.js
@@ -0,0 +1,18 @@
+// Route Paths:
+
+// Route paths, in combination with a request method,
+// define the endpoints at which requests can be made.
+// Route paths can be strings, string patterns, or regular expressions.
+
+// For eg.:
+// This route path will match requests to /about.
+
+// app.get('/about', function (req, res) {
+//     res.send('about')
+//   })
+
+// This route path will match requests to /random.text.
+
+// app.get('/random.text', function (req, res) {
+//   res.send('random.text')
+// })
